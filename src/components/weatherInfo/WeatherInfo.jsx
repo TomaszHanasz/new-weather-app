92e41c0d5c9463aa41fa1f16a3fae092fb832692
Tomasz Hanasz <tomaszhanasz@yahoo.pl>
2023-06-01 11:49:06 -0500

    initial commit

---
+++ src/components/weatherInfo/WeatherInfo.jsx
@@ -0,0 +1,95 @@
+import React, { useState, useEffect } from "react";
+import "./weatherInfo.style.css";
+import sun from "../../images/sun.svg";
+import cloudy from "../../images/cloudy.svg";
+import suncloud from "../../images/suncloud.svg";
+import rain from "../../images/rain.svg";
+import snow from "../../images/snow.svg";
+import moon from "../../images/moon.svg";
+import moonCloud from "../../images/mooncloud.svg";
+import thunder from "../../images/thunder.svg";
+
+const WeatherInfo = (props) => {
+  const { city, temperature, description, feelsLike, humidity, wind, icon } =
+    props;
+
+  const [imageSrc, setImageSrc] = useState("");
+
+  useEffect(() => {
+    switch (icon) {
+      case "01d":
+        setImageSrc(sun);
+        break;
+      case "02d":
+        setImageSrc(suncloud);
+        break;
+      case "03d":
+        setImageSrc(cloudy);
+        break;
+      case "04d":
+        setImageSrc(cloudy);
+        break;
+      case "09d":
+        setImageSrc(rain);
+        break;
+      case "10d":
+        setImageSrc(rain);
+        break;
+      case "11d":
+        setImageSrc(thunder);
+        break;
+      case "13d":
+        setImageSrc(snow);
+        break;
+      case "50d":
+        setImageSrc(suncloud);
+        break;
+      case "01n":
+        setImageSrc(moon);
+        break;
+      case "02n":
+        setImageSrc(moonCloud);
+        break;
+      case "03n":
+        setImageSrc(cloudy);
+        break;
+      case "04n":
+        setImageSrc(cloudy);
+        break;
+      case "09n":
+        setImageSrc(rain);
+        break;
+      case "10n":
+        setImageSrc(rain);
+        break;
+      case "11n":
+        setImageSrc(thunder);
+        break;
+      case "13n":
+        setImageSrc(snow);
+        break;
+      case "50n":
+        setImageSrc(moonCloud);
+        break;
+      default:
+        setImageSrc("");
+        break;
+    }
+  }, [icon]);
+
+  return (
+    <div className="weather-info">
+      <h3>{city}</h3>
+      <img src={imageSrc} />
+      <p className="temp">{temperature}℉</p>
+      <div className="description">
+        <p className="description-item">{description}</p>
+        <p className="description-item">Feels like: {feelsLike}℉</p>
+        <p className="description-item">Humidity: {humidity} %</p>
+        <p className="description-item">Wind: {wind} mph</p>
+      </div>
+    </div>
+  );
+};
+
+export default WeatherInfo;
---
+++ src/containers/weatherApp/WeatherApp.jsx
@@ -0,0 +1,83 @@
+import React, { useState } from "react";
+import axios from "axios";
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
+import WeatherInfo from "../../components/weatherInfo/WeatherInfo";
+import "./weatherApp.style.css";
+
+const WeatherApp = () => {
+  const [temperature, setTemperature] = useState(0);
+  const [city, setCity] = useState("");
+  const [icon, setIcon] = useState("");
+  const [feelsLike, setFeelsLike] = useState(0);
+  const [description, setDescription] = useState("");
+  const [humidity, setHumidity] = useState(0);
+  const [wind, setWind] = useState(0);
+  const [searchedCity, setSearchedCity] = useState("");
+
+  const onChangeHandler = (e) => {
+    setSearchedCity(e.target.value);
+  };
+
+  const onKeyDownHandler = (e) => {
+    if (e.key === "Enter") {
+      onClickHandler();
+    }
+  };
+
+  const onClickHandler = async () => {
+    try {
+      const response = await axios.get(
+        `https://api.openweathermap.org/data/2.5/weather?q=${searchedCity}&units=imperial&appid=355cf3bff397cfe55bf144d10da9b2d8`
+      );
+      console.log("response", response);
+      const { data } = response;
+      setCity(data.name);
+      setTemperature(data.main.temp.toFixed(0));
+      setFeelsLike(data.main.feels_like.toFixed(0));
+      setDescription(data.weather[0].description);
+      setHumidity(data.main.humidity);
+      setWind(data.wind.speed);
+      setIcon(data.weather[0].icon);
+      console.log(icon);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <div className="weather-app-container">
+      {city && (
+        <WeatherInfo
+          city={city}
+          humidity={humidity}
+          description={description}
+          feelsLike={feelsLike}
+          wind={wind}
+          temperature={temperature}
+          icon={icon}
+        />
+      )}
+
+      <TextField
+        id="filled-basic"
+        label="Enter city name"
+        variant="filled"
+        onChange={onChangeHandler}
+        value={searchedCity}
+        onKeyDown={onKeyDownHandler}
+        style={{ backgroundColor: "white" }}
+        fullWidth
+      />
+      <Button
+        variant="contained"
+        onClick={onClickHandler}
+        style={{ borderRadius: "0" }}
+      >
+        Get weather
+      </Button>
+    </div>
+  );
+};
+
+export default WeatherApp;
